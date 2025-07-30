37ce8fde98b30ef5f0216f23e4829204280cd726
joshuaddf <jkdodofoli@gmail.com>
2025-07-30 01:39:24 +0000

    Made some small changes

--- lib/views/pages/login_page.dart
+++ lib/views/pages/login_page.dart
@@ -1,80 +1,97 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_canvas/views/widget_tree.dart';
 import 'package:flutter_canvas/views/widgets/hero_widget.dart';
 
 TextEditingController emailController = TextEditingController();
 TextEditingController passwordController = TextEditingController();
+String confirmedEmail = "123";
+String confirmedPassword = "456";
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+  const LoginPage({super.key, required this.title});
+  final String title;
 
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
-  @override
-  void dispose() {
-    emailController.dispose();
-    passwordController.dispose();
-    super.dispose();
-  }
+  // @override
+  // void dispose() {
+  //   emailController.dispose();
+  //   passwordController.dispose();
+  //   super.dispose();
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          children: [
-            HeroWidget(title: "Login"),
-            SizedBox(height: 30.0),
-            TextField(
-              controller: emailController,
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                labelText: "Email",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(15),
-                ),
-              ),
-              onEditingComplete: () {
-                setState(() {});
-              },
-            ),
-            SizedBox(height: 10.0),
-            TextField(
-              controller: passwordController,
-              decoration: InputDecoration(
-                labelText: "Password",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(15),
-                ),
-              ),
-              onEditingComplete: () {
-                setState(() {});
-              },
-            ),
-            SizedBox(height: 30.0),
-            FilledButton(
-              onPressed: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return LoginPage();
-                    },
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                HeroWidget(title: widget.title),
+                SizedBox(height: 30.0),
+                TextField(
+                  controller: emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    labelText: "Email",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
                   ),
-                );
-              },
-              style: FilledButton.styleFrom(
-                minimumSize: Size(double.infinity, 40.0),
-              ),
-              child: Text("Get started"),
+                  onEditingComplete: () {
+                    setState(() {});
+                  },
+                ),
+                SizedBox(height: 10.0),
+                TextField(
+                  controller: passwordController,
+                  decoration: InputDecoration(
+                    labelText: "Password",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                  onEditingComplete: () {
+                    setState(() {});
+                  },
+                ),
+                SizedBox(height: 30.0),
+                ElevatedButton(
+                  onPressed: () {
+                    onLogInPressed();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size(double.infinity, 40.0),
+                  ),
+                  child: Text(widget.title),
+                ),
+                SizedBox(height: 60.0,)
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
+  }
+
+  void onLogInPressed() {
+    if (confirmedEmail == emailController.text &&
+        confirmedPassword == passwordController.text) {
+      Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(
+          builder: (context) {
+            return WidgetTree();
+          },
+        ),
+        (route) => false,
+      );
+    }
   }
 }
--- lib/views/pages/welcome_page.dart
+++ lib/views/pages/welcome_page.dart
@@ -8,56 +8,57 @@ class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Lottie.asset('assets/lotties/welcome.json'),
-            FittedBox(
-              child: Text(
-                "Flutter canvas",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 200.0,
-                  letterSpacing: 50.0,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Lottie.asset('assets/lotties/welcome.json'),
+                FittedBox(
+                  child: Text(
+                    "Flutter Canvas",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 40.0, 
+                      letterSpacing: 5.0,
+                    ),
+                  ),
                 ),
-              ),
-            ),
-            SizedBox(height: 20.0),
-            FilledButton(
-              onPressed: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return LoginPage();
-                    },
+                const SizedBox(height: 20.0),
+                FilledButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const LoginPage(title: "Register",),
+                      ),
+                    );
+                  },
+                  style: FilledButton.styleFrom(
+                    minimumSize: const Size(double.infinity, 50.0), 
                   ),
-                );
-              },
-              style: FilledButton.styleFrom(
-                minimumSize: Size(double.infinity, 40.0)
-              ),
-              child: Text("Get started"),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return LoginPage();
-                    },
+                  child: const Text("Get Started"),
+                ),
+                const SizedBox(height: 10.0),
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const LoginPage(title: "Log In",),
+                      ),
+                    );
+                  },
+                  style: TextButton.styleFrom(
+                    minimumSize: const Size(double.infinity, 50.0), // Fixed property
                   ),
-                );
-              },
-              style: TextButton.styleFrom(
-                maximumSize: Size(double.infinity, 40.0)
-              ),
-              child: Text("Log in"),
+                  child: const Text("Log In"),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
